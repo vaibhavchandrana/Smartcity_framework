70af471d79f45b55ad155b9f939e8e48eccc1024
vaibhavchandrana <vaibhavrana8900@gmail.com>
2023-06-04 22:38:59 +0530

    modifying header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -8,7 +8,24 @@ const Header = () => {
     if (profile) {
         isEmployee = profile.employee;
     }
+    const [isNavOpen, setIsNavOpen] = useState(false);
 
+    const handleNavToggle = () => {
+      setIsNavOpen(!isNavOpen);
+    };
+    useEffect(() => {
+        const handleResize = () => {
+          if (window.innerWidth >= 768) {
+            setIsNavOpen(false);
+          }
+        };
+    
+        window.addEventListener('resize', handleResize);
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+    
     return (
         // <div>
         <>
@@ -19,12 +36,16 @@ const Header = () => {
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SmartCity</span>
                     </Link>
                     <div className="flex md:order-2">
-                        <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                        <button  type="button"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded={isNavOpen}
+              onClick={handleNavToggle}>
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
                         </button>
                     </div>
-                    <div className="items-center ml-auto justify-end  hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    <div className={`items-center ml-auto justify-end w-full md:flex md:w-auto md:order-1 ${isNavOpen ? '' : 'hidden'}`} id="navbar-sticky">
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <Link to='/user_login' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Login</Link>
