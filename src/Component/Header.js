9093d3b85bb8b57f4fe387de6ebcab9dcaf70ab0
vaibhavchandrana <vaibhavrana8900@gmail.com>
2023-06-04 22:45:41 +0530

    mofidying header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -9,23 +9,40 @@ const Header = () => {
         isEmployee = profile.employee;
     }
     const [isNavOpen, setIsNavOpen] = useState(false);
-
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const handleNavToggle = () => {
-      setIsNavOpen(!isNavOpen);
+        setIsNavOpen(!isNavOpen);
     };
     useEffect(() => {
         const handleResize = () => {
-          if (window.innerWidth >= 768) {
-            setIsNavOpen(false);
-          }
+            if (window.innerWidth >= 768) {
+                setIsNavOpen(false);
+            }
         };
-    
+
         window.addEventListener('resize', handleResize);
         return () => {
-          window.removeEventListener('resize', handleResize);
+            window.removeEventListener('resize', handleResize);
         };
-      }, []);
-    
+    }, []);
+    useEffect(() => {
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, []);
+
+    const handleOutsideClick = (event) => {
+        if (event.target.id !== 'dropdownNavbarLink') {
+            setIsDropdownOpen(false);
+        }
+    };
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
     return (
         // <div>
         <>
@@ -36,11 +53,11 @@ const Header = () => {
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SmartCity</span>
                     </Link>
                     <div className="flex md:order-2">
-                        <button  type="button"
-              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-sticky"
-              aria-expanded={isNavOpen}
-              onClick={handleNavToggle}>
+                        <button type="button"
+                            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            aria-controls="navbar-sticky"
+                            aria-expanded={isNavOpen}
+                            onClick={handleNavToggle}>
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
                         </button>
@@ -56,35 +73,57 @@ const Header = () => {
                             <li>
                                 <Link to='/' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</Link>
                             </li>
-                            {isEmployee?<li>
+                            {isEmployee ? <li>
                                 <Link to='/profile' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"> Profile</Link>
-                            </li>:null}
-                            {isEmployee?<li>
-                                <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Add Details <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
+                            </li> : null}
+                            {isEmployee ? <li>
+                                <button
+                                    id="dropdownNavbarLink"
+                                    onClick={toggleDropdown}
+                                    className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                                >
+                                    Add Details{' '}
+                                    <svg
+                                        className="w-5 h-5 ml-1"
+                                        aria-hidden="true"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                </button>
                                 {/* <!-- Dropdown menu --> */}
-                                <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                                        <li>
-                                            <Link to='/addHome' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add Home</Link>
-                                        </li>
-                                        <li>
-                                            <Link to='/addSociety' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add Society</Link>
-                                        </li>
-                                        <li>
-                                            <Link to='/add_electricity_transaction' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add Electricity Transaction</Link>
-                                        </li>
-                                        <li>
-                                            <Link to='/add_water_transaction' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add water Transaction</Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>:null}
-                            {isEmployee==false?<li>
+                                {isDropdownOpen && (
+                                    <div
+                                        id="dropdownNavbar"
+                                        className="z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+                                    >                                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                                            <li>
+                                                <Link to='/addHome' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add Home</Link>
+                                            </li>
+                                            <li>
+                                                <Link to='/addSociety' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add Society</Link>
+                                            </li>
+                                            <li>
+                                                <Link to='/add_electricity_transaction' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add Electricity Transaction</Link>
+                                            </li>
+                                            <li>
+                                                <Link to='/add_water_transaction' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add water Transaction</Link>
+                                            </li>
+                                        </ul>
+                                    </div>)}
+                            </li> : null}
+                            {isEmployee == false ? <li>
                                 <Link to='/user_profile' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">User Profile</Link>
-                            </li>:null}
-                            {isEmployee?<li>
+                            </li> : null}
+                            {isEmployee ? <li>
                                 <Link to='/user_list' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">User List</Link>
-                            </li>:null}
+                            </li> : null}
                             <li>
                                 <Link to='/logout' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Logout</Link>
                             </li>
